5e67632408c1ed37cec23acff4119c0a07bd1c15
Shai Michaeli <shaiws2@gmail.com>
2019-01-09 01:31:12 +0200

    Add files via upload

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            resources.ApplyResources(this.button2, "button2");
+            this.button2.Name = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            resources.ApplyResources(this.textBox1, "textBox1");
+            this.textBox1.Name = "textBox1";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // webBrowser1
+            // 
+            resources.ApplyResources(this.webBrowser1, "webBrowser1");
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser1_DocumentCompleted);
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
+            // 
+            // label4
+            // 
+            resources.ApplyResources(this.label4, "label4");
+            this.label4.Name = "label4";
+            // 
+            // label5
+            // 
+            resources.ApplyResources(this.label5, "label5");
+            this.label5.Name = "label5";
+            // 
+            // label6
+            // 
+            resources.ApplyResources(this.label6, "label6");
+            this.label6.Name = "label6";
+            // 
+            // label7
+            // 
+            resources.ApplyResources(this.label7, "label7");
+            this.label7.Name = "label7";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::WindowsFormsApplication1.Properties.Resources.images1;
+            resources.ApplyResources(this.pictureBox2, "pictureBox2");
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            resources.ApplyResources(this.pictureBox1, "pictureBox1");
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // checkBox1
+            // 
+            resources.ApplyResources(this.checkBox1, "checkBox1");
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // button3
+            // 
+            resources.ApplyResources(this.button3, "button3");
+            this.button3.Name = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using System.Diagnostics;
+using System.Net;
+using System.Xml;
+
+namespace WindowsFormsApplication1
+{
+
+    public partial class Form1 : Form
+    {
+        static int count = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            webBrowser1.Navigate("http://www.youtube-mp3.org/");
+            button1.Enabled = false;
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            String text = webBrowser1.DocumentText;
+            button2.Enabled = false;
+            if (text.Contains("submit-form"))
+                button1.Enabled = true;
+            else
+            {
+                button1.Enabled = false;
+                MessageBox.Show("אירעה שגיאהת נסה שוב מאוחר יותר", "שגיאה", MessageBoxButtons.OK, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1, MessageBoxOptions.RightAlign);
+            }
+        }
+        string videoId = null;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Document.GetElementById("youtube-url").SetAttribute("value", textBox1.Text);
+            webBrowser1.Document.GetElementById("submit").InvokeMember("click");
+
+            if (!textBox1.Text.Equals(""))
+            {
+                if (textBox1.Text.StartsWith("https://www.youtube.com/watch?v=") || textBox1.Text.StartsWith("http://www.youtube.com/watch?v="))
+                {
+                    HtmlElement el = webBrowser1.Document.GetElementById("title");
+                    string s = el.InnerText; // get the : Title: **Developers**
+                    Console.WriteLine(s);
+                    String[] text1 = textBox1.Text.Split('=');
+                    text1[0] = s;
+                    Console.WriteLine(text1[0]);
+                    String img = text1[1];
+                    videoId = text1[1];
+                    Console.WriteLine(videoId);
+                    if (img.Equals(""))
+                        MessageBox.Show("כתובת וידאו לא תקנית", "שגיאה", MessageBoxButtons.OK, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1, MessageBoxOptions.RightAlign);
+                    else
+                    {
+                        try
+                        {
+                            pictureBox1.Load("http://i.ytimg.com/vi/" + img + "/default.jpg");
+                        }
+                        catch (SystemException exc)
+                        {
+                            pictureBox1.Image = null;
+                            ShowError(MessageBoxOptions.RightAlign);
+                            Console.WriteLine(exc.ToString());
+                        }
+                        // button1.Enabled = false;
+                        button2.Enabled = true;
+
+                    }
+                }
+                else
+                    MessageBox.Show("כתובת וידאו לא תקנית\nכתובת תקנית חייבת להתחיל כמו בדוגמא\nhttps://www.youtube.com/watch?v=", "שגיאה", MessageBoxButtons.OK, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1, MessageBoxOptions.RightAlign);
+            }
+            else
+                MessageBox.Show("לא ניתן להשאיר שדה כתובת ריק", "שגיאה", MessageBoxButtons.OK, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1, MessageBoxOptions.RightAlign);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            HtmlElement dl_link = webBrowser1.Document.GetElementById("dl_link");
+            HtmlElementCollection links = dl_link.GetElementsByTagName("a");
+            String url = null;
+            try
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    if (links[i].GetAttribute("href").Contains("get?ab="))
+                    {
+                        url = links[i].GetAttribute("href");
+                        break;
+                    }
+                }
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            try
+            {
+                WebClient download = new WebClient();
+                SaveFileDialog savefile = new SaveFileDialog();
+                savefile.FileName = videoId;
+                savefile.Filter = "MP3 files (*.mp3)|*.mp3|All files (*.*)|*.*";
+                if (savefile.ShowDialog() == DialogResult.OK)
+                    download.DownloadFile(url, savefile.FileName);
+            }
+            catch
+            {
+                ShowError(MessageBoxOptions.RightAlign);
+            }
+        }
+        public void ShowError(MessageBoxOptions options)
+        {
+            MessageBox.Show(".אירעה שגיאה, נסה שוב מאוחר יותר\nשגיאה זו נגרמת בדרך כלל עקב בעיות זכויות יוצרים או מפאת אורכו של הווידאו\nאנו תומכים רק בקטעי וידאו עם מקסימום של 20 דקות", "שגיאה", MessageBoxButtons.OK, MessageBoxIcon.Error, MessageBoxDefaultButton.Button1, options);
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+                this.TopMost = true;
+            if (!checkBox1.Checked)
+                this.TopMost = false;
+        }
+
+        public void PasteLink()
+        {
+            String link = Clipboard.GetText();
+            if (link.Contains("youtube.com"))
+                textBox1.Text = link;
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            PasteLink();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            button1.Enabled = true;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image != null)
+                System.Diagnostics.Process.Start(textBox1.Text);
+
+        }
+
+        private String BindVideo(string videoId)
+        {
+            string sViewCount = string.Empty;
+            string sTitle = string.Empty;
+            bool bPublished = false;
+            bool bTitle = false;
+            try
+            {
+                XmlTextReader reader = new XmlTextReader("http://gdata.youtube.com/feed/api/videos/" + videoId);
+                while (reader.Read())
+                {
+                    switch (reader.NodeType)
+                    {
+                        case XmlNodeType.Element:
+                            {
+                                switch (reader.Name)
+                                {
+                                    case "published":
+                                        bPublished = true;
+                                        break;
+                                    case "yt:statistics":
+                                        {
+                                            while (reader.MoveToNextAttribute())
+                                            {
+                                                if (reader.Name.ToLower().Equals("viewcount"))
+                                                    sViewCount = reader.Value;
+                                            }
+                                        }
+                                        break;
+                                    case "media:title":
+                                        bTitle = true;
+                                        break;
+                                }
+                                break;
+                            }
+                        case XmlNodeType.Text:
+                            if (bPublished)
+                            {
+                               var dtPublishDate = Convert.ToDateTime(reader.Value);
+                            }
+                            else if (bTitle)
+                            {
+                                sTitle = reader.Value;
+                                Console.WriteLine(sTitle);
+                            }
+                            break;
+                        case XmlNodeType.EndElement:
+                            {
+                                bPublished = false;
+                                bTitle = false;
+                                break;
+                            }
+                    }
+                }
+                reader.Close();
+                Console.WriteLine(sViewCount);
+                Console.WriteLine(sTitle);
+                Console.WriteLine(bTitle);
+                Console.WriteLine(bPublished);
+            }
+            catch { }
+            return sTitle;
+        }
+       
+    }
+}
